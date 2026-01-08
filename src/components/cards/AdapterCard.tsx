6adc897476a595a011286a0772437ae3312440de
mvriu5 <marius.ahsmus@gmail.com>
2026-01-08 15:12:06 +0100

    removed lottielabs

--- src/components/cards/AdapterCard.tsx
+++ src/components/cards/AdapterCard.tsx
@@ -1,23 +1,13 @@
 "use client"
 
-import React from "react"
-import dynamic from 'next/dynamic';
-import {useTranslations} from "next-intl"
-
-const PlayerWithNoSSR = dynamic(() => import('@lottielab/lottie-player/react').then(module => module.default), {ssr: false})
+import { useTranslations } from "next-intl";
+import React from "react";
 
 export const AdapterCard: React.FC = () => {
     const t = useTranslations("FeatureSection")
 
     return (
         <div className={"flex flex-col justify-between items-center overflow-hidden gap-4 p-4 h-[420px] col-span-3 row-span-2 bg-[#050316] rounded-xl border border-white/10"}>
-
-            <PlayerWithNoSSR
-                src={'https://cdn.lottielab.com/l/7Y49BdSnCisP5m.json'}
-                style={{ height: '400px', width: '400px' }}
-                autoplay
-                loop
-            />
 
             <div className={"flex flex-col gap-1"}>
                 <p className={"font-mono font-semibold text-lg text-white/75"}>
