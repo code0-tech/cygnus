b24adf5ae76dac8934ff6f115e5e07d6b9e2dde6
Marius Ahsmus <marius.ahsmus@gmail.com>
2026-01-16 16:31:09 +0100

    fix: added mdx loader to next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    turbopack: {
+          rules: {
+            "*.mdx": {
+              loaders: ["@mdx-js/loader"],
+              as: "*.js",
+            },
+          },
+        },
 }
 
 const withMDX = createMDX({})
