609fe12bd62710cb5d5102a48654ffa1d4d338bf
Marius Ahsmus <marius.ahsmus@gmail.com>
2026-01-16 16:54:06 +0100

    fix: useOutsideClick hook definition - initial missing

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -2,8 +2,8 @@ import {RefObject, useEffect, useRef} from "react"
 
 type EventHandler = (e: MouseEvent | TouchEvent) => void;
 
-function useOutsideClick<T extends HTMLElement = HTMLElement>(callback: EventHandler): RefObject<T> {
-    const ref = useRef<T>()
+function useOutsideClick<T extends HTMLElement = HTMLElement>(callback: EventHandler): RefObject<T | null> {
+    const ref = useRef<T>(null)
 
     useEffect(() => {
         const handle = (e: MouseEvent | TouchEvent) => {
