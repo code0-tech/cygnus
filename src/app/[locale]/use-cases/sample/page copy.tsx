9a3bff65bcd353af0f415d43249c0254d80d1d45
mvriu5 <marius.ahsmus@gmail.com>
2026-01-19 10:50:03 +0100

    fix: removed copy of page

--- src/app/[locale]/use-cases/sample/page copy.tsx
+++
@@ -1,9 +0,0 @@
-import { LandingContainer } from "@/components/LandingContainer"
-
-export default function UseCaseSamplePage() {
-    return (
-        <LandingContainer>
-            <p>Test</p>
-        </LandingContainer>
-    )
-}
