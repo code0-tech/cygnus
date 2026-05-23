b94e693397a2ff1ad2218c79db4c62ba6e1ead4d
Marius Ahsmus <marius.ahsmus@gmail.com>
2026-05-23 05:23:23 +0200

    fix: remove force-dynamic

--- src/app/(landing)/[locale]/layout.tsx
+++ src/app/(landing)/[locale]/layout.tsx
@@ -13,7 +13,6 @@ interface LocaleLayoutProps {
 }
 
 export const revalidate = 300
-export const dynamic = "force-dynamic"
 
 export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
     const { locale } = await params
